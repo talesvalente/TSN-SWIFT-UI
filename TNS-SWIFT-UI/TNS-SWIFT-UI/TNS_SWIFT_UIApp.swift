f4465e130b0ed3dae03b72784ad0370eb10730f0
Tales Valente <talesvalente@gmail.com>
2022-08-10 11:50:03 -0300

    MASTER CONTENT
    UPDATE

---
+++ TNS-SWIFT-UI/TNS-SWIFT-UI/TNS_SWIFT_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TNS_SWIFT_UIApp.swift
+//  TNS-SWIFT-UI
+//
+//  Created by Tales Valente on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TNS_SWIFT_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
